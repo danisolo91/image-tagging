9029d8ab6fd130361a3080c708b1b77b29ffb102
Daniel <danisolo91@gmail.com>
2021-04-16 23:11:51 +0200

    App implementation

--- src/App.js
+++ src/App.js
@@ -1,39 +1,71 @@
-import firebase from 'firebase/app';
-import { useEffect, useState } from 'react';
 import img from './image.jpg';
 import Start from './components/start';
+import Congratulations from './components/congratulations';
+import Scores from './components/scores';
 import Top from './components/top';
+import { useContext, useRef } from 'react';
+import { GameContext } from './contexts/GameContext';
 
 const App = () => {
 
-    const [state, setState] = useState({ characters: null });
+    const [game, setGame] = useContext(GameContext);
+    const modal = useRef(null);
+    const image = useRef(null);
 
-    useEffect(() => {
-        firebase.firestore().collection('characters')
-        .onSnapshot(serverUpdate => {
-            // firebase will run this everytime the collection is updated
-            const characters = serverUpdate.docs.map(_character => {
-                const data = _character.data();
-                data['id'] = _character.id;
-                return data;
-            });
-            console.log(characters);
-            setState({ characters: characters });
+    const checkClick = (e) => {
+        let x = e.pageX;
+        let y = e.pageY;
+        let id = null;
+        let found = 0;
+        
+
+        game.characters.forEach(_character => {
+            if(_character.found) found = found + 1;
+            if(x >= _character.minX && x <= _character.maxX &&
+            y >= _character.minY && y <= _character.maxY) {
+                id = _character.id;
+                found = found + 1;
+            }
         });
-    }, []);
+
+        if(id) {
+            let endTime = null;
+
+            if(found === 3) {
+                endTime = new Date();
+            }
+
+            setGame(prevState => {
+                return {
+                    ...prevState,
+                    player: {
+                        ...prevState.player,
+                        endTime: endTime,
+                    },
+                    characters: prevState.characters.map(_ch => {
+                        if(_ch.id === id) _ch.found = true;
+                        return _ch;
+                    }),
+                };
+            });
+            alert('very good!');
+        } else {
+            alert('Noup!');
+        }
+    };
 
     return (
         <>
             <Top />
-            <div className="image">
-                <img src={img} alt="tag the characters" />
-            </div>
-            <div id="modal" className="modal">
-                <Start />
+            <img className="image" onClick={checkClick} ref={image} src={img} alt="tag the characters" />
+            <div ref={modal} id="modal" className="modal">
                 {
-                    // <Congratulations />
-                    // <Scores />
+                    game.player.startTime === null ?
+                        <Start modal={modal.current} /> :
+                        (game.player.endTime && !game.gameOver) &&
+                            <Congratulations modal={modal.current} />
                 }
+                {game.gameOver && <Scores modal={modal.current} />}
             </div>
         </>
     );
